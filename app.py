e6e753e52bfd7b68edace2e222d3ef4f5eb542ac
Anjelica <ambrosio.anjelica@gmail.com>
2019-10-08 15:15:01 -0700

    updated

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 import requests
 import json
-from random import choice
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+from datetime import datetime
 import os
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Animals')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
+animals = db.animals
 
 app = Flask(__name__)
 
@@ -11,10 +18,16 @@ def index():
     """Return homepage."""
     return render_template('home.html', msg='Welcome to Fox Finder!')
 
-@app.route('/animals')
-def playlists_index():
-    """Show all playlists."""
-    return render_template('playlists_index.html', playlists=playlists.find())
+@app.route('/animals/<animal_id>')
+def show_animal(animal_id):
+    """Show a single animal."""
+    animal = animals.find_one({'_id': ObjectId(animal_id)})
+    return render_template('show_animal.html', animal=animal)
+
+# @app.route('/animals')
+# def playlists_index():
+#     """Show all playlists."""
+#     return render_template('playlists_index.html', playlists=playlists.find())
 
 
 
