1f4025000a67869fc0006ae8144db436976e5593
Anjelica <ambrosio.anjelica@gmail.com>
2019-10-08 22:46:44 -0700

    updated file

--- app.py
+++ app.py
@@ -6,17 +6,23 @@ from pymongo import MongoClient
 from datetime import datetime
 import os
 
-host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Animals')
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/myanimals')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
-animals = db.animals
+animals = db.myanimals
+
 
 app = Flask(__name__)
 
-@app.route('/')
+# @app.route('/')
 def index():
     """Return homepage."""
-    return render_template('home.html', msg='Welcome to Fox Finder!')
+    return render_template('home.html', msg="Welcome to Noah's Ark!" )
+
+@app.route('/')
+def animals_index():
+    """Show all animals."""
+    return render_template('animals_index.html',animals=animals.find())
 
 @app.route('/animals/<animal_id>',methods=['GET'])
 def show_animal(animal_id):
@@ -24,10 +30,7 @@ def show_animal(animal_id):
     animal = animals.find_one({'_id': ObjectId(animal_id)})
     return render_template('show_animal.html', animal=animal)
 
-# @app.route('/animals')
-# def playlists_index():
-#     """Show all playlists."""
-#     return render_template('playlists_index.html', playlists=playlists.find())
+
 
 
 
