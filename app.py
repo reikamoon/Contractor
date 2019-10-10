2c94aedde49f3a2f254aab456c930d9b9fbfe4fc
Anjelica <ambrosio.anjelica@gmail.com>
2019-10-10 09:01:14 -0700

    fixed formatting and syntax errors

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/myanimals')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 animals = db.myanimals
-
-animals.insert_many= ([
-    {"name" : "Sylvain", "species" : "Fox", "breed" : "Red Fox", "color" : "Red", "price" : "1500", "img_url": "../static/images/foxrz.jpg"},
-    {"name": "Dimitri", "species": "Lion", "breed": "Panthera Leo Melanochaita", "color": "Blond", "price": "$3500", "img_url": "../static/images/lionrz.jpg"},
-    {"name": "Felix", "species": "Wolf", "breed": "Gray Wolf", "color": "Gray", "price": "2500", "img_url": "../static/images/wolfrz.jpg"},
-    {"name" : "Minerva", "species" : "Hamster", "breed" : "Syrian Hamster", "color" : "Beige", "price" : "250", "img_url": "../static/images/hamsterrz.jpg"},
-    {"name" : "Mika", "species" : "Dog", "breed" : "Schnauzer", "color" : "Gray", "price" : "priceless", "img_url": "../static/images/schanuzerrz.jpg"}
-
-])
+#Database not working? Tried to replicate it here?
+# animals.insert_many= ([
+#     {"name" : "Sylvain", "species" : "Fox", "breed" : "Red Fox", "color" : "Red", "price" : "1500", "img_url": "../static/images/foxrz.jpg"},
+#     {"name": "Dimitri", "species": "Lion", "breed": "Panthera Leo Melanochaita", "color": "Blond", "price": "$3500", "img_url": "../static/images/lionrz.jpg"},
+#     {"name": "Felix", "species": "Wolf", "breed": "Gray Wolf", "color": "Gray", "price": "2500", "img_url": "../static/images/wolfrz.jpg"},
+#     {"name" : "Minerva", "species" : "Hamster", "breed" : "Syrian Hamster", "color" : "Beige", "price" : "250", "img_url": "../static/images/hamsterrz.jpg"},
+#     {"name" : "Mika", "species" : "Dog", "breed" : "Schnauzer", "color" : "Gray", "price" : "priceless", "img_url": "../static/images/schanuzerrz.jpg"}
+#
+# ])
 
 app = Flask(__name__)
 
